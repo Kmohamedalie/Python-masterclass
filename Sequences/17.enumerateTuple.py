b2c259a9456d53aee5a4b54d3689be2b71cbf556
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2024-02-27 01:05:23 +0100

    Delete Sequences directory

--- Sequences/17.enumerateTuple.py
+++
@@ -1,7 +0,0 @@
-# *****************************************************
-#                enumerate  Tuples                    #
-# *****************************************************
-
-for t in enumerate("abcdefgh"):
-    index, character = t
-    print(index, character)
